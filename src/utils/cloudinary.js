4743d8ca0c62db67bf599ee02a51d91886aad1d7
kaif-nisar <kaifquest786@gmail.com>
2026-05-25 12:36:52 +0530

    updated cloudinary file

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,11 +1,10 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs/promises"; // Use promises for cleaner asynchronous file handling
 
-// Configure Cloudinary
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // Function to upload a file to Cloudinary
